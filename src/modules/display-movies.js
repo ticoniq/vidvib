fd14445527eb4c25b73803f88a8abdc43feb2884
ticoniq_ <okolietochi@gmail>
2023-06-26 19:08:06 +0100

    Updated display-movies.js

--- src/modules/display-movies.js
+++ src/modules/display-movies.js
@@ -9,23 +9,22 @@ const displayList = async () => {
 
     const result = response.data;
     const movies = result.splice(0, 24);
-    let output = '';
     movies.forEach((movie) => {
       const img = document.createElement('img');
       img.setAttribute('src', heart);
       img.className = 'like-icon';
-      output += `
-      <div class="card">
-        <img src="${movie.image.medium}" alt="${movie.name}" class="image">
-        <h3>${movie.name}</h3>
-        <div class="card-body">
-          <button>Comment</button>
-          <p>${img.outerHTML} Likes</p>
-        </div>
+      const card = document.createElement('div');
+      card.className = 'card';
+      card.innerHTML = `
+      <img src="${movie.image.medium}" alt="${movie.name}" class="image">
+      <h3>${movie.name}</h3>
+      <div class="card-body">
+        <button>Comment</button>
+        <p>${img.outerHTML} Likes</p>
       </div>
       `;
+      displayMovies.appendChild(card);
     });
-    displayMovies.innerHTML = output;
   } catch (error) {
     console.error('Error:', error);
   }
